ed7ac1a1ab1eaf800e38859cc822f1e38c3a79f6
Mehtab-Ali <rmehtabali@gmail.com>
2024-09-16 20:16:43 -0400

    html file

--- players.py
+++ players.py
@@ -1,1 +1,13 @@
-print("Hello")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.futbin.com/24/squad-building-challenges"
+html = requests.get(url)
+
+s = BeautifulSoup(html.content, "html.parser")
+
+results = s.find(id="content-container")
+sbc_name = results.find_all("div", class_="sbc-name bold")
+
+for name in sbc_name:
+    print(name.text)
